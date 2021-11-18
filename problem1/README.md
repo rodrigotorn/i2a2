de704df23ad4fcb2e65549c6e5cac67b06974516
Rodrigo Torn <rodrigo.torn@outlook.com>
2021-11-17 22:38:24 -0300

    chore: Prepare the environment for development

---
+++ problem1/README.md
@@ -0,0 +1,5 @@
+# Problem 1
+
+This problem is proposed based on the introdutory classes about Probability, Statistics and Linear Algebra.
+
+The goal is to transform a latin text composed by 4 paragraphs and 350 words into a matrix.
---
+++ problem1/pipeline.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Problem 1
+#
+# Transform a text input into a matrix
+
+# %%
